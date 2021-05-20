efa07d8f301441b61458f374955e1850f58e915b
Juliana Fajardo <juliana.fajardo92@gmail.com>
2021-05-20 15:16:33 -0500

    cambios

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -30,22 +30,23 @@ export default class Introduction extends Component {
                       <div className="slider-text-inner">
                         <div className="desc">
                           <h1> And <br />Graphic designer</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://github.com/wjulifajarb?tab=repositories" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
+                          <p><a className="btn btn-primary btn-learn" href="https://github.com/wjulifajarb?tab=repositories" target="_blank" rel="noopener noreferrer">View Projects</a></p>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </li>
-                <li style={{backgroundImage: 'url(images/img_bg2.png)'}}>
+               
+              <li style={{backgroundImage: 'url(images/img_bg3.png)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1> And <br />Graphic designer</h1>
-                          <p><a className="btn btn-primary btn-learn" href="www.linkedin.com/in/julianafajardo" target="_blank" rel="noopener noreferrer">View LinkedIN <i className="icon-briefcase3" /></a></p>
+                          <h1> And <br/>Political scientist </h1>
+                          <p><a className="btn btn-primary btn-learn" href="https://scienti.minciencias.gov.co/cvlac/visualizador/generarCurriculoCv.do?cod_rh=0001763383" target="_blank" rel="noopener noreferrer">View CVlac </a></p>
                         </div>
                       </div>
                     </div>
@@ -60,22 +61,7 @@ export default class Introduction extends Component {
                       <div className="slider-text-inner">
                         <div className="desc">
                           <h1> And <br/>Political scientist </h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://scienti.minciencias.gov.co/cvlac/visualizador/generarCurriculoCv.do?cod_rh=0001763383" target="_blank" rel="noopener noreferrer">View CVlac <i className="icon-book" /></a></p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li style={{backgroundImage: 'url(images/img_bg3.png)'}}>
-                <div className="overlay" />
-                <div className="container-fluid">
-                  <div className="row">
-                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                      <div className="slider-text-inner">
-                        <div className="desc">
-                          <h1> And <br/>Political scientist </h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://orcid.org/0000-0003-1894-6526" target="_blank" rel="noopener noreferrer">View OrcID <i className="icon-book" /></a></p>
+                          <p><a className="btn btn-primary btn-learn" href="https://orcid.org/0000-0003-1894-6526" target="_blank" rel="noopener noreferrer">View OrcID</a></p>
                         </div>
                       </div>
                     </div>
